d432a03b1b60aa33d3b71c0ea697a7a43c24ec5d
unknown <sandaruwan@alphagrid.com>
2025-12-02 00:10:47 +0530

    6Fixes + improvements after Vercel build errors2

---
+++ types/turf.d.ts
@@ -0,0 +1,9 @@
+declare module '@turf/turf' {
+  export function booleanPointInPolygon(point: any, polygon: any): boolean;
+  export const helpers: any;
+  export const meta: any;
+  // Add other turf functions as needed
+  const turf: any;
+  export default turf;
+}
+
